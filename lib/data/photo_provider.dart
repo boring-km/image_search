4fdeb13b5b1386e302f79821fd40e182d55127fc
boring-km <kms0644804@naver.com>
2022-01-06 07:01:01 +0900

    06 Stream을 활용하여 데이터 다루기

--- lib/data/photo_provider.dart
+++ lib/data/photo_provider.dart
@@ -1,10 +1,15 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:image_search/data/api.dart';
+import 'package:image_search/model/photo.dart';
 
 class PhotoProvider extends InheritedWidget {
   final PixabayApi api;
+  final _photoStreamController = StreamController<List<Photo>>()..add([]);
+  Stream<List<Photo>> get photoStream => _photoStreamController.stream;
 
-  const PhotoProvider({
+  PhotoProvider({
     Key? key,
     required this.api,
     required Widget child,
@@ -14,6 +19,11 @@ class PhotoProvider extends InheritedWidget {
     final PhotoProvider? result = context.dependOnInheritedWidgetOfExactType<PhotoProvider>();
     assert(result != null, 'No PixabayApi found in context');
     return result!;
+  }
+
+  Future<void> fetch(String query) async {
+    final result = await api.fetch(query);
+    _photoStreamController.add(result);
   }
 
   @override
